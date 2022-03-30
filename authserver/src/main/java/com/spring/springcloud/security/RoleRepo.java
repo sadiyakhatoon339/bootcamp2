d7bd94e4fc2b8ca4639561f44a1153b1354b6f74
sadiyakhatoon339 <72649215+sadiyakhatoon339@users.noreply.github.com>
2022-03-30 20:38:52 +0530

    Add files via upload

---
+++ authserver/src/main/java/com/spring/springcloud/security/RoleRepo.java
@@ -0,0 +1,8 @@
+package com.spring.springcloud.security;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface RoleRepo extends JpaRepository<Role, Long> {
+
+}
---
+++ couponservice/src/main/java/com/spring/springcloud/security/SecurityService.java
@@ -0,0 +1,6 @@
+package com.spring.springcloud.security;
+
+public interface SecurityService {
+
+	boolean login(String userName,String password);
+}
